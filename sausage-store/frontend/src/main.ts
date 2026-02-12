a2d0e56bbe785bdd8b834e79385ad1a3cb6cdde5
Okami312 <baltaglaurentiu99@gmail.com>
2026-02-12 13:58:18 +0200

    moved comments

--- sausage-store/frontend/src/main.ts
+++ sausage-store/frontend/src/main.ts
@@ -11,6 +11,3 @@ if (environment.production) {
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.log(err));
-
-// this code it's ok but i need it in React pls
-// if you have any questions don't hesitate to ask me

7bbca95e370f70fca0b0781b2bc94931e55b215b
Okami312 <baltaglaurentiu99@gmail.com>
2026-02-12 13:28:26 +0200

    am creat hello.txt

--- sausage-store/frontend/src/main.ts
+++ sausage-store/frontend/src/main.ts
@@ -12,4 +12,5 @@ platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.log(err));
 
-// hello this is a comment
+// this code it's ok but i need it in React pls
+// if you have any questions don't hesitate to ask me
